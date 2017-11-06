80a612c3afa0bfbdcbbbda4c50b7a9fcbe7ffa87
Dm <ofisser86@gmail.com>
2017-11-06 15:57:56 +0100

    Update 2-1-7.py

--- stepic_py_basic_use/w2/2-1-7.py
+++ stepic_py_basic_use/w2/2-1-7.py
@@ -112,3 +112,30 @@ for _ in range(int(input())):
             break
     if addToList:
         classesOrder.append(className)
+        #===============================================================================================
+ n = int(input())
+classes = {}
+for i in range(n):
+    line = input()
+    parts = line.split(" : ")
+    cls = parts[0]
+    if len(parts) == 1:
+        classes[cls] = []
+    else:
+        classes[cls] = parts[1].split(" ")
+
+
+def check(src, dest):
+    if src == dest:
+        return True
+    return any([check(child, dest) for child in classes[src]])
+
+
+m = int(input())
+used = []
+
+for i in range(m):
+    cls = input()
+    if any([check(cls, used_one) for used_one in used]):
+        print(cls)
+    used.append(cls)
